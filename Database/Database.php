047bb12903954faa3c8ab74368c8a438823922cf
Omar-Codess <omartaki8@gmail.com>
2022-12-13 18:44:08 +0100

    creato array "products"

--- Database/Database.php
+++ Database/Database.php
@@ -1,1 +1,16 @@
 <?php
+require_once __DIR__ . "/../Models/Category.php";
+require_once __DIR__ . "/../Models/Food.php";
+require_once __DIR__ . "/../Models/Toy.php";
+
+$cat = new Category("cat");
+$dog = new Category("dog");
+
+$mouse = new Toy("mouse", 5, $cat);
+$bone = new Toy("bone", 10, $dog);
+
+$products = [
+    $mouse,
+    $bone
+];
+var_dump($products);
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once __DIR__ . "/Database/database.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
