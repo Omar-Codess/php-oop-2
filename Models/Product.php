444255dc3dda97c3e58f345fdbba64f560e21e6e
Omar-Codess <omartaki8@gmail.com>
2022-12-13 18:00:22 +0100

    creato costruttore in product

---
+++ Models/Product.php
@@ -0,0 +1,13 @@
+<?php 
+class Product {
+    function __construct(public string $name, public float $price, public Category $category)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->category = $category;
+    }
+
+    public function getDetails() {
+        return "name: $this->name";
+    }
+}
